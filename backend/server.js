721afd55c5c92bb3674dd4c1fccb3904b6828c7a
Subhash Adhikari <subhash@Subhashs-MacBook-Air.local>
2025-06-06 20:48:53 +0545

    Added Auth route in backend and tested

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+
 
 // Load .env variables
 dotenv.config();
@@ -9,6 +11,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', authRoutes);
 
 // Test route
 app.get('/', (req, res) => {
